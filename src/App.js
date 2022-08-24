aff2eac39df9477bfb5e305e42ab88f3aa647e8e
EslamMohamed74 <44731543+EslamMohamed74@users.noreply.github.com>
2022-08-25 01:21:13 +0200

    fix route and localstorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ConfigureStore } from "./redux";
 
 import Header from "./components/Header/Header";
@@ -15,7 +15,7 @@ const store = ConfigureStore();
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/fudex_task">
         <React.Fragment>
           <Header />
           <Routes>
@@ -23,6 +23,7 @@ const App = () => {
             <Route exact path="/products/:id" element={<ProductDetail />} />
             <Route exact path="/cart" element={<ShoppingCart />} />
             <Route exact path={"/order"} element={<OrderNowForm />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
           <Footer />
         </React.Fragment>
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,44 +1,46 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import shop from './reducers/shopReducer';
-
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import shop from "./reducers/shopReducer";
 
 // convert object to string and store in localStorage
 const saveToLocalStorage = (state) => {
-    try {
-        const serialisedState = JSON.stringify(state);
-        localStorage.setItem("persistantState", serialisedState);
-    } catch (e) {
-        console.warn(e);
-    }
-}
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+};
 
 // load string from localStarage and convert into an Object
 // invalid output must be undefined
 const loadFromLocalStorage = () => {
-    try {
-        const serialisedState = localStorage.getItem("persistantState");
-        if (serialisedState === null) return undefined;
-        return JSON.parse(serialisedState);
-    } catch (e) {
-        console.warn(e);
-        return undefined;
-    }
-}
-
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (
+      serialisedState === null ||
+      serialisedState?.shop?.products?.length === 0
+    )
+      return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+};
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            shop
-        }),
-        loadFromLocalStorage(),
-        compose(
-            applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-
-    );
-    store.subscribe(() => saveToLocalStorage(store.getState()));
-    return store;
-}
+  const store = createStore(
+    combineReducers({
+      shop,
+    }),
+    loadFromLocalStorage(),
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+  store.subscribe(() => saveToLocalStorage(store.getState()));
+  return store;
+};
